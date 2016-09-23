8907d3db2b7285ec29f5b0b79615bddd3c0452b3
Fabien <fabien.webdev@gmail.com>
2016-09-23 18:24:32 +0900

    Custom Sign Service
    Simple calculation app: enter a text and it gets its length and calculates the corresponding price. Using Javascript to update result on submit.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+/****************************************
+********** Custom Sign Script ***********
+****************************************/
 
+// Define all variables
+
+var price;
+var total;
+var signValue;
+var signLength;
+
+// Define the price
+price = 5;
+
+// Execute this function when user send the form
+function updateValue(){
+  // Get the text the user entered
+  signValue = document.forms["signForm"]['sign'].value;
+  // Get its length
+  signLength = signValue.length;
+
+  // Calculate the price
+  total = price * signLength;
+
+  // Change the price in the red banner
+  var elCost = document.getElementById('cost');
+  elCost.textContent = '$' + total;
+
+  // Change the preview text
+  var elSign = document.getElementById('preview');
+  elSign.textContent = signValue;
+}
