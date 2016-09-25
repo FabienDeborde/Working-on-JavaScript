a05830637c5e84d1d3f7af424b6dbae224be9838
Fabien <fabien.webdev@gmail.com>
2016-09-25 11:18:01 +0900

    Added second script
    Added a check order script with its markup and styling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/****************************************
-********** Custom Sign Script ***********
-****************************************/
+/******************************************************
+********** First script: Custom Sign Script ***********
+*******************************************************/
 
 // Define all variables
 
@@ -30,3 +30,48 @@ function updateValue(){
   var elSign = document.getElementById('preview');
   elSign.textContent = signValue;
 }
+
+//////////////////////////////////////////////////////////////////
+
+/******************************************************
+********** Second script: Order check Script ***********
+*******************************************************/
+
+//Create variables for the welcome message
+var greeting = 'Hello ';
+var name = 'Myna';
+var message = ', please check your order:';
+  //Concatenate the 3 variables to create the message
+var welcome = greeting + name + message;
+
+//Create variables to hold details about the sign
+var sign = 'My awesome sign';
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+
+//Get the element that has an id of greeting
+var el = document.getElementById('greeting');
+//Replace its content with the personalized message
+el.textContent = welcome;
+
+//Get the element that has an id of userSign then update its contents
+var elSign = document.getElementById('userSign');
+elSign.textContent = sign;
+
+//Get the element that has an id of tiles then update its contents
+var elTiles = document.getElementById('tiles');
+elTiles.textContent = tiles;
+
+//Get the element that has an id of subTotal then update its contents
+var elSubTotal = document.getElementById('subTotal');
+elSubTotal.textContent = '$' + subTotal;
+
+//Get the element that has an id of shipping then update its contents
+var elShipping = document.getElementById('shipping');
+elShipping.textContent = '$' + shipping;
+
+//Get the element that has an id of grandTotal then update its contents
+var elGrandTotal = document.getElementById('grandTotal');
+elGrandTotal.textContent = '$' + grandTotal;
