2cc1f6c450acd9e7e814aa107f599802c302537c
Fabien <fabien.webdev@gmail.com>
2016-10-19 12:11:30 +0900

    More json

--- JSON/scripts.js
+++ JSON/scripts.js
@@ -35,7 +35,7 @@ function quotes(){
     var newContent = '<div class="quote"><table class="table table-inverse table-striped table-bordered table-hover">';
     newContent += '<thead><tr><th>#</th><th>Name</th><th>Quote</th></tr></thead><tbody>';
 
-    for (var i = 0; i < responseObject.quotes.length; i++) {
+    for (var i = 0; i < 10; i++) {
       newContent += '<tr>';
       newContent += '<th scope="row">' + responseObject.quotes[i].id + '</th>';
       newContent += '<td>' + responseObject.quotes[i].author + '</td>';
@@ -44,8 +44,6 @@ function quotes(){
     }
     newContent += '</tbody></table></div>';
 
-    console.log(newContent);
-
     document.getElementById('quotesContainer').innerHTML = newContent;
   };
 
@@ -53,3 +51,53 @@ function quotes(){
   xhr.send(null);
 
 };
+
+// Load HTML with jQuery
+
+$('a#jqueryLink').on('click', function(e){
+  e.preventDefault(); // prevent the link to load a new page
+  var url = this.href; // get the url from the link
+
+  $('#jqueryHTML').load(url); // load the page in the selected element
+})
+
+// Load JSON with jQuery
+
+loadJson();
+
+function loadJson(){
+  $.getJSON('quotes.json')
+  .done(function(quotes){
+    var msg = '<div class="quote"><table class="table table-inverse table-striped table-bordered table-hover">';
+    msg += '<thead><tr><th>#</th><th>Name</th><th>Quote</th></tr></thead><tbody>';
+
+    $.each(quotes, function(index, val){
+      $.each(val, function(i, v){
+        msg += '<tr>';
+        msg += '<th scope="row">' + v.id + '</th>';
+        msg += '<td>' + v.author + '</td>';
+        msg += '<td>' + v.quote + '</td>';
+        msg += '</tr>';
+      });
+    });
+
+    msg += '</tbody></table></div>';
+    $('#jsonQuotes').html(msg);
+  }).fail(function(){
+    $('#jsonQuotes').text("Couldn't access the JSON file")
+  });
+}
+
+
+/*
+
+
+for (var i = 0; i < responseObject.quotes.length; i++) {
+  newContent += '<tr>';
+  newContent += '<th scope="row">' + responseObject.quotes[i].id + '</th>';
+  newContent += '<td>' + responseObject.quotes[i].author + '</td>';
+  newContent += '<td>' + responseObject.quotes[i].quote + '</td>';
+  newContent += '</tr>';
+}
+newContent += '</tbody></table></div>';
+*/
