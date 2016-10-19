507f1d9c520e61945e402e94f5ad21ab550ab9d4
Fabien <fabien.webdev@gmail.com>
2016-10-19 18:47:46 +0900

    Finished my work on JSON and API

--- JSON/scripts.js
+++ JSON/scripts.js
@@ -1,3 +1,9 @@
+// Scroll to bottom
+$('html, body').animate({
+  scrollTop: $(document).height()+ 3500},
+  1000,
+  'swing'
+);
 // Events
 events();
 function events(){
@@ -89,15 +95,94 @@ function loadJson(){
 }
 
 
-/*
+// AJAX query with jQuery
+
+$('a#ajaxLink').on('click', function(e){
+  e.preventDefault();
+  $.ajax({
+    type:"GET",
+    url: 'quotes.json',
+    timeout: 2000,
+    beforeSend: function(){
+      $('h2#ajaxTitle').after('<div class="loading"> Loading... </div>');
+    },
+    complete: function(){
+      $('.loading').remove();
+    },
+    success: function(quotes){
+      var msg = '<div class="quote"><table class="table table-inverse table-striped table-bordered table-hover">';
+      msg += '<thead><tr><th>#</th><th>Name</th><th>Quote</th></tr></thead><tbody>';
+      $.each(quotes, function(index, val){
+        $.each(val, function(i, v){
+          msg += '<tr>';
+          msg += '<th scope="row">' + v.id + '</th>';
+          msg += '<td>' + v.author + '</td>';
+          msg += '<td>' + v.quote + '</td>';
+          msg += '</tr>';
+        });
+      });
+
+      msg += '</tbody></table></div>';
+      $('#ajaxContainer').html(msg);
+    },
+    error: function(){
+      $('#ajaxContainer').html('<div class="error"> An error occured. Please try again later... </div>')
+    }
+
+  });
 
 
-for (var i = 0; i < responseObject.quotes.length; i++) {
-  newContent += '<tr>';
-  newContent += '<th scope="row">' + responseObject.quotes[i].id + '</th>';
-  newContent += '<td>' + responseObject.quotes[i].author + '</td>';
-  newContent += '<td>' + responseObject.quotes[i].quote + '</td>';
-  newContent += '</tr>';
+});
+
+// Store data with HTML API
+
+storeAPI();
+
+function storeAPI(){
+  localStorage.setItem('town', 'Owariasahi');
+  localStorage.setItem('country', 'Japan');
+
+  var msg = 'data stored :' + localStorage.length + '</br>';
+
+  $.each(localStorage, function(index, value){
+    msg += index + ': ' + value + '<br/>';
+  })
+  $('#storageAPI').html(msg)
+
 }
-newContent += '</tbody></table></div>';
-*/
+
+
+// External API and JSON
+ajaxReq();
+
+function ajaxReq() {
+  $.ajax({
+    type: "GET",
+    url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+    crossDomain: true,
+    dataType: 'jsonp',
+    timeout: 2000,
+    beforeSend: function(){
+      $('h2#API').after('<div class="loading"> Loading... </div>');
+    },
+    complete: function(){
+      $('.loading').remove();
+    },
+    success: function(data){
+      console.log(data);
+      var msg = '';
+      msg += '<span>" ' + data.quoteText + ' "</span>';
+      msg += '<br><span>' + data.quoteAuthor + '</span>';
+      $('#externalAPI').html(msg);
+    },
+    error: function(data){
+      $('#externalAPI').html('<div class="error"> An error occured. Please try again later... </div>');
+    }
+  });
+};
+
+
+$('#randomQuote').on('click', function(e){
+  e.preventDefault();
+  ajaxReq();
+})
